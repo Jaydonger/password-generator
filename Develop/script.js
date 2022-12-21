e819b355b6cb64670fe38a9da767a2ab0612f572
Jordan Pace <jordanpace95@gmail.com>
2022-12-20 19:32:17 -0700

    finished pw generator js code

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,35 @@ var generateBtn = document.querySelector("#generate");
 var numbers = '0123456789';
 var symbols = '!@#$%^&*()_+=<>';
 var letters = 'abcdefghijklmnopqrstuvwxyz';
+var capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var userNumbers;
+var userSymbols;
+var userCap;
+var length;
 // Write password to the #password input
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function generatePassword() {
+  retVal = "";
+  for(i = 0, n = letters.length; i < length; i++){
+    retVal += letters.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+
+}
 
 function writePassword() {
-  var userNumbers = prompt('Would you like your password to have numbers? (yes or no)');
+  // prompts user for length of password.
+  letters = 'abcdefghijklmnopqrstuvwxyz';
+  length = prompt('Enter a number between 8 and 128 for your passwords length.');
+  // checks that number falls between 8 and 128.
+    while(length < 8 || length > 128) {
+      length = prompt('That number is invalid. Please enter a number between 8 and 128.');
+    }
+  
+  // prompts user asking if they want numbers in their password.
+  userNumbers = prompt('Would you like your password to have numbers? (yes or no)');
     if(userNumbers !== 'Yes' && userNumbers !== 'Y' && userNumbers !== 'No' && userNumbers !== 'N') {
       userNumbers = capitalizeFirstLetter(userNumbers);
       if(userNumbers !== 'Yes' && userNumbers !== 'Y' && userNumbers !== 'No' && userNumbers !== 'N') {
@@ -24,9 +46,34 @@ function writePassword() {
     // check to see if .concat worked.
     console.log(letters);
 
-    //var userSymbols = prompt('Would you like your password to have special symbols? (yes or no') {
-      
-    //}
+
+    // prompts user for symbols
+    userSymbols = prompt('Would you like your password to have special symbols? (yes or no')
+    userSymbols = capitalizeFirstLetter(userSymbols);
+      while(userSymbols !== 'Yes' && userSymbols !== 'Y' && userSymbols !== 'No' && userSymbols !== 'N') {
+        alert('Please enter a valid response.');
+        userSymbols = prompt('Would you like your password to have special symbols? (yes or no')
+        userSymbols = capitalizeFirstLetter(userSymbols);
+      }
+
+    // adds symbols variables to letters variable if user chose yes.
+    if(userSymbols === 'Yes' || userSymbols === 'Y' || userSymbols === 'yes' || userSymbols === 'y') {
+      letters = letters.concat(symbols);
+    }
+
+    console.log(letters);
+
+    userCap = prompt('Would you like your password to contain capital letters as well? (yes or no)')
+    userCap = capitalizeFirstLetter(userCap);
+    while(userCap !== 'Yes' && userCap !== 'Y' && userCap !== 'No' && userCap !== 'N') {
+      alert('Please enter a valid response.');
+      userCap = prompt('Would you like your password to contain capital letters as well? (yes or no)')
+      userCap = capitalizeFirstLetter(userCap);
+    }
+
+    if(userCap === 'Yes' || userCap === 'Y' || userCap === 'yes' || userCap === 'y') {
+      letters = letters.concat(capLetters);
+    }
 
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -34,6 +81,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
