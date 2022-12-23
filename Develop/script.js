8968ba43eec547995a313d7d430a1aa45edd142e
Jordan Pace <jordanpace95@gmail.com>
2022-12-22 22:58:11 -0700

    updated the readme file

--- Develop/script.js
+++ Develop/script.js
@@ -12,19 +12,21 @@ var length;
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// Add functionality to generate password function call.
 function generatePassword() {
   retVal = "";
   for(i = 0, n = letters.length; i < length; i++){
     retVal += letters.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
-
 }
 
 function writePassword() {
   // prompts user for length of password.
   letters = 'abcdefghijklmnopqrstuvwxyz';
   length = prompt('Enter a number between 8 and 128 for your passwords length.');
+
   // checks that number falls between 8 and 128.
     while(length < 8 || length > 128) {
       length = prompt('That number is invalid. Please enter a number between 8 and 128.');
@@ -32,18 +34,18 @@ function writePassword() {
   
   // prompts user asking if they want numbers in their password.
   userNumbers = prompt('Would you like your password to have numbers? (yes or no)');
+  userNumbers = capitalizeFirstLetter(userNumbers);
     if(userNumbers !== 'Yes' && userNumbers !== 'Y' && userNumbers !== 'No' && userNumbers !== 'N') {
-      userNumbers = capitalizeFirstLetter(userNumbers);
-      if(userNumbers !== 'Yes' && userNumbers !== 'Y' && userNumbers !== 'No' && userNumbers !== 'N') {
-        alert('Please enter a valid response');
-        writePassword();
-      }
+      alert('Please enter a valid response');
+      writePassword();
     }
+
     // adds numbers variable to letters variable if user chose yes
     if(userNumbers === 'Yes' || userNumbers === 'Y' || userNumbers === 'yes' || userNumbers === 'y') {
       letters = letters.concat(numbers);
     }
-    // check to see if .concat worked.
+
+    // check to see if variables were added or not.
     console.log(letters);
 
 
@@ -61,8 +63,10 @@ function writePassword() {
       letters = letters.concat(symbols);
     }
 
+    // check to see that symbols was added or not.
     console.log(letters);
 
+    // prompt user for capital letters.
     userCap = prompt('Would you like your password to contain capital letters as well? (yes or no)')
     userCap = capitalizeFirstLetter(userCap);
     while(userCap !== 'Yes' && userCap !== 'Y' && userCap !== 'No' && userCap !== 'N') {
@@ -74,6 +78,9 @@ function writePassword() {
     if(userCap === 'Yes' || userCap === 'Y' || userCap === 'yes' || userCap === 'y') {
       letters = letters.concat(capLetters);
     }
+
+    // check to see that capital letters were added or not.
+    console.log(letters);
 
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
